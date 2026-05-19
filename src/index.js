d9765e49bca635615f8528ae42f00f0aed8cd4c4
AhmedRaza186 <sallumoon650@gmail.com>
2026-05-19 14:19:37 +0500

    typo mistake solved

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ app.get('/api/health', (req, res) => {
 if (!process.env.PRODUCTION) {
   app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    return
   })
 }
 
